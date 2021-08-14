f8686aebcb811eaca104b4468a4fda0d5574e50c
Rafael <Rafabatera-8@hotmail.com>
2021-08-14 02:23:45 -0300

    questions finalized

--- frontend/src/app/components/views/questions/questions.component.ts
+++ frontend/src/app/components/views/questions/questions.component.ts
@@ -9,10 +9,34 @@ export class QuestionsComponent implements OnInit {
 
   cards = [
     {
-      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............'
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............',
+      totalComments: 10,
+      author: 'Pedro',
+      tag: 'React Js'
     },
     {
-      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............'
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............',
+      totalComments: 10,
+      author: 'Pedro',
+      tag: 'Angular 11'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............',
+      totalComments: 10,
+      author: 'Pedro',
+      tag: 'PHP'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............',
+      totalComments: 10,
+      author: 'Pedro',
+      tag: 'Vue Js'
+    },
+    {
+      title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatisLorem ipsum dolor sit amet, consectetur adipiscing elit ut............',
+      totalComments: 10,
+      author: 'Pedro',
+      tag: 'ReactNative Js'
     }
   ]
   constructor() { }
