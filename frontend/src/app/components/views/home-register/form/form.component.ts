d4c7e07bc0aee01d65298092f1cade7038f6e363
Rafael <Rafabatera-8@hotmail.com>
2021-08-14 16:15:41 -0300

    confirm

--- frontend/src/app/components/views/home-register/form/form.component.ts
+++ frontend/src/app/components/views/home-register/form/form.component.ts
@@ -1,44 +1,69 @@
-import {User} from './user'
-import { Component, Input, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from './user'
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
+
+  user = <User>{
+    name: '',
+    password: '',
+    email: '',
+    phone: null
+  }
   formContainer = [
     {
       name: 'Nome',
       placeholder: 'Samuel do Amor',
-      type: 'text'
+      type: 'text',
+      date: String
     },
     {
       name: 'Senha',
       placeholder: '******************',
-      type: 'password'
+      type: 'password',
+      date: String
     },
     {
       name: 'Email',
       placeholder: 'name@example.com',
-      type: 'email'
+      type: 'email',
+      date: String
     },
     {
       name: 'Telefone',
       placeholder: '(91) 9836-2202',
-      type: 'text'
+      type: 'text',
+      date: Number
     }
   ]
-  @Input() user: User = {
-    name: 'asddas',
-    password: '',
-    email:'',
-    phone: null
-  }
 
-  constructor() { }
+
+  constructor(private router: Router, private crudService: CrudService) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  console() {
+
+    this.formContainer.forEach(element => {
+    console.log(element.date)
+      
+    });
+
+  }
+  createUser() {
+    this.crudService.newUser(this.user).subscribe(()=>{
+      this.router.navigate(['/login'])
+    }, err =>{
+      console.log(err)
+    })
+  }
 }
